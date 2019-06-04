2978fad32d863102be66da11b98963a2e5e7102c
markvb84 <51340535+markvb84@users.noreply.github.com>
2019-06-04 16:19:28 +0200

    Update CheckWinConditions.java

--- CheckWinConditions.java
+++ CheckWinConditions.java
@@ -1,65 +1,12 @@
-import java.util.Arrays;
-
 public class CheckWinConditions {
-    
-    private Grid grid;
-    
-    public CheckWinConditions(Grid grid){
-        this.grid = grid;
-    }
-    
-        public boolean checkWinner(){
-        if(this.grid.onPosition(0,0).equals("X") && this.grid.onPosition(1,1).equals("X") && this.grid.onPosition(2,2).equals("X")){
-            return true;
-        }
-        if(this.grid[0][2] == "X" && this.grid[1][1] == "X" && this.grid[2][0] == "X"){
-            return true;
-        }
-        if(this.grid[0][0] == "X" && this.grid[0][1] == "X" && this.grid[0][2] == "X"){
-            return true;
-        }
-        if(this.grid[1][0] == "X" && this.grid[1][1] == "X" && this.grid[1][2] == "X"){
-            return true;
-        }
-        if(this.grid[2][0] == "X" && this.grid[2][1] == "X" && this.grid[2][2] == "X"){
-            return true;
-        }
-        if(this.grid[0][0] == "X" && this.grid[1][0] == "X" && this.grid[2][0] == "X"){
-            return true;
-        }
-        if(this.grid[0][1] == "X" && this.grid[1][1] == "X" && this.grid[2][1] == "X"){
-            return true;
-        }
-        if(this.grid[0][2] == "X" && this.grid[1][2] == "X" && this.grid[2][2] == "X"){
-            return true;
-        }
-        
-        if(this.grid[0][0] == "O" && this.grid[1][1] == "O" && this.grid[2][2] == "O"){
-            return true;
-        }
-        if(this.grid[0][2] == "O" && this.grid[1][1] == "O" && this.grid[2][0] == "O"){
-            return true;
-        }
-        if(this.grid[0][0] == "O" && this.grid[0][1] == "O" && this.grid[0][2] == "O"){
-            return true;
-        }
-        if(this.grid[1][0] == "O" && this.grid[1][1] == "O" && this.grid[1][2] == "O"){
-            return true;
-        }
-        if(this.grid[2][0] == "O" && this.grid[2][1] == "O" && this.grid[2][2] == "O"){
-            return true;
-        }
-        if(this.grid[0][0] == "O" && this.grid[1][0] == "O" && this.grid[2][0] == "O"){
-            return true;
-        }
-        if(this.grid[0][1] == "O" && this.grid[1][1] == "O" && this.grid[2][1] == "O"){
-            return true;
-        }
-        if(this.grid[0][2] == "O" && this.grid[1][2] == "O" && this.grid[2][2] == "O"){
+
+    public static boolean checkWinner(Grid grid){
+        if(grid.onPosition(0,0).equals("X") && grid.onPosition(1,1).equals("X") && grid.onPosition(2,2).equals("X")){
             return true;
         }
         else{
             return false;
         }
+
     }
 }
