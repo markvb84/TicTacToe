5e8ee1d0a2aa6ffb9dab94563d5884bbefe0b8ea
markvb84 <51340535+markvb84@users.noreply.github.com>
2019-06-04 22:07:54 +0200

    Update NewGame.java

--- NewGame.java
+++ NewGame.java
@@ -2,13 +2,16 @@ public class NewGame {
 
     public static void main(String[] args) {
 
-        Grid test = new Grid();
-
-        test.insertX(1, 0);
-
-        System.out.println(test.onPosition(1, 1));
-        CheckWinConditions.checkWinner(test);
-
-        String blah[][] = test.getGrid();
+        
+        
+//        test.insertX(1, 1);
+//        test.insertX(2, 2);
+        //System.out.println(test.onPosition(1, 1));
+        Grid test = new Grid();    
+        test.insertX(2, 1);
+        test.insertO(0, 1);
+        test.insertX(1, 2);
+        
+        GridRenderer.drawGrid(test);
     }
 }
