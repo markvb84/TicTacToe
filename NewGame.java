0a1c0cc4e272825d35dfed63a863d03c06201d35
markvb84 <51340535+markvb84@users.noreply.github.com>
2019-06-04 16:20:34 +0200

    Update NewGame.java

--- NewGame.java
+++ NewGame.java
@@ -1,10 +1,14 @@
-import java.util.Arrays;
-
 public class NewGame {
-    
-        public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
+        Grid test = new Grid();
 
+        test.insertX(1, 0);
+
+        System.out.println(test.onPosition(1, 1));
+        CheckWinConditions.checkWinner(test);
+
+        String blah[][] = test.getGrid();
     }
 }
