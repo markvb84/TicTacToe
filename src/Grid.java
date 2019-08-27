109e18bfb523d547faba2d73b93c8e09fcd197d8
markvb84 <51340535+markvb84@users.noreply.github.com>
2019-08-27 15:33:57 +0200

    Update Grid.java
    Refactored exception handling

--- src/Grid.java
+++ src/Grid.java
@@ -2,7 +2,7 @@ public class Grid {
 
     private String[][] grid;
 
-    public Grid(){
+    public Grid() {
         this.grid = new String[3][3];
 
 //        00 01 02
@@ -10,49 +10,37 @@ public class Grid {
 //        20 21 22
     }
 
-    public String[][] getGrid() {
-        return grid;
-    }
-
     public String onPosition(int x, int y) {
-        if(grid[y][x] == null){
+        if (grid[y][x] == null) {
             return " ";
-        }
-        else{
+        } else {
             return grid[y][x];
         }
     }
 
     public boolean insertX(int y, int x) {
-        try{
-            if(this.grid[y][x] == null){
+        try {
+            if (this.grid[y][x] == null) {
+                this.grid[y][x] = "X";
+                return true;
+            } else {
+                return false;
             }
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            return false;
-        }
-        if(this.grid[y][x] == null){
-            this.grid[y][x] = "X";
-            return true;
-        }
-        else{
+        } catch (ArrayIndexOutOfBoundsException e) {
             return false;
         }
     }
 
-    public boolean insertO(int y, int x){
-        try{
-            if(this.grid[y][x] == null){
+
+    public boolean insertO ( int y, int x){
+        try {
+            if (this.grid[y][x] == null) {
+                this.grid[y][x] = "O";
+                return true;
+            } else {
+                return false;
             }
-        }
-        catch (ArrayIndexOutOfBoundsException e) {
-            return false;
-        }
-        if(this.grid[y][x] == null){
-            this.grid[y][x] = "O";
-            return true;
-        }
-        else{
+        } catch (ArrayIndexOutOfBoundsException e) {
             return false;
         }
     }
